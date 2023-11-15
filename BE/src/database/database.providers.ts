bb4cdf2d31feae5f5b9e9c180c7b508ef09329ad
yaongmeow <minnie_00@naver.com>
2023-11-15 15:13:31 +0900

    add: TypeORM 연동
    Co-authored-by: kmi0817 <kmi0817@naver.com>

---
+++ BE/src/database/database.providers.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      });
+      return dataSource.initialize();
+    },
+  },
+];
