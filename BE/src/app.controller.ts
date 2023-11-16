2f83a813bb78b88da8b348cf0c4d75794caea3f2
kmi0817 <kmi0817@naver.com>
2023-11-16 18:45:53 +0900

    add: Nest와 ObjectStorage 연동
    Co-authored-by: yaongmeow <minnie_00@naver.com>

--- BE/src/app.controller.ts
+++ BE/src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { StorageService } from './storage/storage.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly storageService: StorageService
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async upload(@UploadedFile() file: Express.Multer.File) {
+    return this.storageService.upload(file);
   }
 }
