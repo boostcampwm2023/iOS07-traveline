1cee35ffc622e13b8b72a279514bf16c066fb7f2
김영인 <0inn1220@gmail.com>
2023-11-16 18:31:57 +0900

    feat #34: EnvPlugin 분리 및 ConfigPlugin 추가

---
+++ iOS/Plugins/EnvPlugin/Plugin.swift
@@ -0,0 +1,10 @@
+//
+//  Plugin.swift
+//  EnvPlugin
+//
+//  Created by 김영인 on 2023/11/16.
+//
+
+import ProjectDescription
+
+let envPlugin = Plugin(name: "EnvPlugin")
--- iOS/Tuist/Config.swift
+++ iOS/Tuist/Config.swift
@@ -2,6 +2,7 @@ import ProjectDescription
 
 let config = Config(
     plugins: [
-        .local(path: .relativeToManifest("../../Plugins/IOS")),
+        .local(path: .relativeToManifest("../../Plugins/EnvPlugin")),
+        .local(path: .relativeToManifest("../../Plugins/ConfigPlugin"))
     ]
 )
