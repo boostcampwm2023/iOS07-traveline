80ab21182afce2c29406cbcc3914a0e90cb2fc14
yaongmeow <minnie_00@naver.com>
2023-11-19 02:15:52 +0900

    chore: 의존성 문제 해결을 위해 Auth 모듈의 Providers에 StorgeService 추가

--- BE/src/auth/auth.module.ts
+++ BE/src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { UsersService } from 'src/users/users.service';
+import { StorageService } from 'src/storage/storage.service';
 
 @Module({
   imports: [UsersModule],
   controllers: [AuthController],
-  providers: [AuthService, UsersService],
+  providers: [AuthService, UsersService, StorageService],
 })
 export class AuthModule {}
