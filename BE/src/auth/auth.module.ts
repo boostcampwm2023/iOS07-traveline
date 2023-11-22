f87d324c0f0865d2bf59a9046071e5e877a4a995
yaongmeow <minnie_00@naver.com>
2023-11-22 20:55:19 +0900

    feat: auth 모듈에서 JWT 모듈 import

--- BE/src/auth/auth.module.ts
+++ BE/src/auth/auth.module.ts
@@ -4,9 +4,17 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { UsersService } from 'src/users/users.service';
 import { StorageService } from 'src/storage/storage.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: 'traveline_is_the_best', //.env에 넣어서 이용하고 싶었는데,,, 뭔가 문제가 있는지 읽어오는데 문제가 있네요ㅠㅠ
+      signOptions: { expiresIn: '900s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UsersService, StorageService],
 })
