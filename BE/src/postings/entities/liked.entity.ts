96a70d87c76edb8ceb20ca0428baf37ce6e8a9ff
kmi0817 <kmi0817@naver.com>
2023-11-22 12:18:53 +0900

    refactor: 좋아요에 is_deleted 필드 추가 & 회원/게시글 삭제 시 좋아요/신고 기록도 삭제되도록 설정
    좋아요 직접 삭제 시엔 soft delete로, 회원 탈퇴나 게시글 삭제 시엔 좋아요 hard delete로 설정했다.

--- BE/src/postings/entities/liked.entity.ts
+++ BE/src/postings/entities/liked.entity.ts
@@ -1,6 +1,6 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Posting } from './posting.entity';
-import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class Liked {
@@ -10,16 +10,17 @@ export class Liked {
   @PrimaryColumn({ type: 'char', length: 36 })
   posting: string;
 
+  @Column({ name: 'is_deleted', type: 'bool', default: false })
+  isDeleted: boolean;
+
   @ManyToOne(() => User, (user) => user.likeds, {
     onDelete: 'CASCADE',
-    onUpdate: 'CASCADE',
   })
   @JoinColumn({ name: 'user', referencedColumnName: 'id' })
   users: User;
 
   @ManyToOne(() => Posting, (posting) => posting.likeds, {
     onDelete: 'CASCADE',
-    onUpdate: 'CASCADE',
   })
   @JoinColumn({ name: 'posting', referencedColumnName: 'id' })
   postings: Posting;
