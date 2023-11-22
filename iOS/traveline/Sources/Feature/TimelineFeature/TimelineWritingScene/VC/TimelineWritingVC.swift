57c4ff4cfa3ce4a51d45f097d50252829a182e16
otoolz <otool.hf@gmail.com>
2023-11-23 00:45:52 +0900

    feat #89: 타임라인 작성 뷰 UI

---
+++ iOS/traveline/Sources/Feature/TimelineFeature/TimelineWritingScene/VC/TimelineWritingVC.swift
@@ -0,0 +1,147 @@
+//
+//  TimelineWritingVC.swift
+//  traveline
+//
+//  Created by KiWoong Hong on 2023/11/22.
+//  Copyright © 2023 traveline. All rights reserved.
+//
+
+import UIKit
+
+final class TimelineWritingVC: UIViewController {
+    
+    private enum Metric {
+        static let topInset: CGFloat = 24
+        static let margin: CGFloat = 16
+        static let spacing: CGFloat = 20
+        static let belowLine: CGFloat = 4
+    }
+    
+    private enum Constants {
+        static let titlePlaceholder: String = "제목 *"
+        static let contentPlaceholder: String = "내용을 입력해주세요. *"
+        static let complete: String = "완료"
+    }
+    
+    // MARK: - UI Components
+    
+    private let titleTextField: TitleTextField = .init()
+    private let dateLabel: TLLabel = .init(font: TLFont.body2, color: TLColor.gray)
+    private let selectTime: TLImageLabel = .init(image: TLImage.Travel.time, text: "현재시각")
+    private let selectLocation: TLImageLabel = .init(image: TLImage.Travel.location, text: "선택한 장소")
+    private let selectImageButton: SelectImageButton = .init()
+    private let textView: UITextView = {
+        let view = UITextView()
+        view.text = Constants.contentPlaceholder
+        view.textColor = TLColor.disabledGray
+        view.font = TLFont.body1.font
+        view.backgroundColor = TLColor.black
+        
+        return view
+    }()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupAttributes()
+        setupLayout()
+        dateLabel.setText(to: "2023년 11월 9일")
+    }
+    
+    // MARK: - Functions
+    
+    @objc func selectImageButtonTapped() {
+        selectImageButton.setImage(UIImage(systemName: "leaf") ?? nil)
+    }
+    
+    @objc func completeButtonTapped() {
+        // action
+    }
+    
+}
+
+// MARK: - Setup Functions
+
+private extension TimelineWritingVC {
+    
+    func setupAttributes() {
+        view.backgroundColor = TLColor.black
+        titleTextField.placeholder = Constants.titlePlaceholder
+        textView.delegate = self
+        let imageTapGesture = UITapGestureRecognizer(target: self, action: #selector(selectImageButtonTapped))
+        selectImageButton.addGestureRecognizer(imageTapGesture)
+        selectImageButton.isUserInteractionEnabled = true
+    }
+    
+    private func setupNavigationItem() {
+        self.navigationItem.title = "Day01"
+        let completeButton = UIBarButtonItem(
+            title: Constants.complete,
+            style: .plain,
+            target: self,
+            action: #selector(completeButtonTapped)
+        )
+        completeButton.isEnabled = false
+        completeButton.setTitleTextAttributes([.font: TLFont.body1.font], for: .normal)
+        completeButton.setTitleTextAttributes([.foregroundColor: TLColor.gray], for: .disabled)
+        completeButton.setTitleTextAttributes([.foregroundColor: TLColor.main], for: .normal)
+        self.navigationItem.rightBarButtonItem = completeButton
+    }
+    
+    func setupLayout() {
+        view.addSubviews(
+            titleTextField,
+            dateLabel,
+            selectTime,
+            selectLocation,
+            selectImageButton,
+            textView
+        )
+        
+        view.subviews.forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Metric.margin).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Metric.margin).isActive = true
+        }
+        
+        NSLayoutConstraint.activate([
+            titleTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Metric.topInset),
+            dateLabel.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: Metric.belowLine),
+            selectTime.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: Metric.spacing),
+            selectLocation.topAnchor.constraint(equalTo: selectTime.bottomAnchor, constant: Metric.spacing),
+            selectImageButton.topAnchor.constraint(equalTo: selectLocation.bottomAnchor, constant: Metric.spacing),
+            textView.topAnchor.constraint(equalTo: selectImageButton.bottomAnchor, constant: Metric.spacing),
+            textView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+    }
+}
+
+// MARK: - UITextViewDelegate
+
+extension TimelineWritingVC: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == TLColor.disabledGray {
+            textView.text = nil
+            textView.textColor = TLColor.white
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = Constants.contentPlaceholder
+            textView.textColor = TLColor.disabledGray
+        }
+    }
+}
+        
+@available(iOS 17, *)
+#Preview("TimelineWritingVC") {
+    let timelineWritingVC = TimelineWritingVC()
+    let homeNV = UINavigationController(rootViewController: timelineWritingVC)
+    return homeNV
+}
+
---
+++ iOS/traveline/Sources/Feature/TimelineFeature/TimelineWritingScene/View/SelectImageButton.swift
@@ -0,0 +1,183 @@
+//
+//  SelectImageButton.swift
+//  traveline
+//
+//  Created by KiWoong Hong on 2023/11/22.
+//  Copyright © 2023 traveline. All rights reserved.
+//
+
+import UIKit
+
+final class SelectImageButton: UIView {
+    
+    private enum Metric {
+        static let viewWidth: CGFloat = 90
+        static let buttonWidth: CGFloat = 24
+        static let buttonOffset: CGFloat = 9
+        static let iconWidth: CGFloat = 30
+        static let iconSpacing: CGFloat = 20
+        static let cornuerRadius: CGFloat = 12
+    }
+    
+    // MARK: - UI Components
+    
+    let view: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = Metric.cornuerRadius
+        view.clipsToBounds = true
+        
+        return view
+    }()
+    
+    private let cancelButton: UIButton = {
+        let button = UIButton()
+        button.setImage(TLImage.Common.closeBlack, for: .normal)
+        button.tintColor = TLColor.white
+        button.layer.cornerRadius = Metric.cornuerRadius
+        button.backgroundColor = TLColor.white
+        
+        return button
+    }()
+    
+    let imageView: UIImageView = .init()
+    
+    let selectView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.alignment = .center
+        view.distribution = .fillProportionally
+        view.spacing = -4
+        
+        return view
+    }()
+    
+    let selectViewIcon: UIImageView = {
+        let view = UIImageView(image: TLImage.Common.album)
+        view.contentMode = .scaleAspectFit
+        
+        return view
+    }()
+    
+    let selectViewLabel: TLLabel = {
+        let label = TLLabel(font: TLFont.body2, color: TLColor.white)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    // MARK: - Properties
+    
+    private var hasImage: Bool {
+        imageView.image != nil
+    }
+    
+    // MARK: - initialize
+    
+    init() {
+        super.init(frame: .zero)
+        
+        setupAttributes()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    
+    @objc func cancelButtonTapped() {
+        imageView.image = nil
+        updateView()
+        
+    }
+    
+    private func updateView() {
+        if hasImage {
+            selectView.isHidden = true
+            imageView.isHidden = false
+            cancelButton.isHidden = false
+        } else {
+            selectView.isHidden = false
+            imageView.isHidden = true
+            cancelButton.isHidden = true
+        }
+    }
+    
+    func setImage(_ image: UIImage?) {
+        imageView.image = image
+        updateView()
+    }
+    
+}
+
+// MARK: - Setup Functions
+
+private extension SelectImageButton {
+    
+    func setupAttributes() {
+        selectViewLabel.setText(to: "선택")
+        view.backgroundColor = TLColor.backgroundGray
+        updateView()
+        cancelButton.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
+    }
+    
+    func setupLayout() {
+        addSubviews(
+            view,
+            cancelButton
+        )
+        view.addSubviews(
+            imageView,
+            selectView
+        )
+        selectView.addArrangedSubviews(
+            selectViewIcon,
+            selectViewLabel
+        )
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        view.subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        selectView.subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        
+        NSLayoutConstraint.activate([
+            view.widthAnchor.constraint(equalToConstant: Metric.viewWidth),
+            view.heightAnchor.constraint(equalToConstant: Metric.viewWidth),
+            view.topAnchor.constraint(equalTo: topAnchor),
+            view.leadingAnchor.constraint(equalTo: leadingAnchor),
+            view.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            cancelButton.widthAnchor.constraint(equalToConstant: Metric.buttonWidth),
+            cancelButton.heightAnchor.constraint(equalToConstant: Metric.buttonWidth),
+            cancelButton.topAnchor.constraint(equalTo: view.topAnchor, constant: -Metric.buttonOffset),
+            cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Metric.buttonOffset),
+            
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            selectView.topAnchor.constraint(equalTo: view.topAnchor),
+            selectView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            selectView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            selectView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            selectViewIcon.topAnchor.constraint(equalTo: selectView.topAnchor, constant: Metric.iconSpacing),
+            selectViewIcon.widthAnchor.constraint(equalToConstant: Metric.iconWidth),
+            selectViewIcon.heightAnchor.constraint(equalToConstant: Metric.iconWidth),
+            selectViewIcon.centerXAnchor.constraint(equalTo: selectView.centerXAnchor),
+            selectViewLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+}
+
+@available(iOS 17, *)
+#Preview("TimelineWritingVC") {
+    let view = UIView()
+    let button = SelectImageButton()
+    view.addSubview(button)
+    
+    button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    
+    return view
+}
