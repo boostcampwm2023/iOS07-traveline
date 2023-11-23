a63f8fbf9f347b6140e281a022370d8acdecabe1
yaongmeow <minnie_00@naver.com>
2023-11-23 22:19:33 +0900

    chore: DTO 이름 변경

--- BE/src/users/dto/user-info.dto.ts
+++ BE/src/users/dto/user-info.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 import { CreateAuthDto } from 'src/auth/dto/create-auth.dto';
 
-export class UpdateUserDto extends PartialType(CreateAuthDto) {
+export class UserInfoDto extends PartialType(CreateAuthDto) {
   @IsOptional()
   @IsString()
   @MinLength(1)
