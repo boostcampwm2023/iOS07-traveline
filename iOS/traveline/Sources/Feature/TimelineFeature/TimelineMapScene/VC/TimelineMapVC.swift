fea109b38eecb52b6c8dad93d37b4d4d98270870
kth1210 <k2hop1210@gmail.com>
2023-11-23 18:42:36 +0900

    feat #112: Timeline 지도로 보기 화면 구현
    TimelineMapVC

---
+++ iOS/traveline/Sources/Feature/TimelineFeature/TimelineMapScene/VC/TimelineMapVC.swift
@@ -0,0 +1,146 @@
+//
+//  TimelineMapVC.swift
+//  traveline
+//
+//  Created by 김태현 on 11/23/23.
+//  Copyright © 2023 traveline. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+final class TimelineMapVC: UIViewController {
+    
+    private enum Metric {
+        static let horizontalInset: CGFloat = 16.0
+    }
+    
+    private enum Constants {
+        static let annotatinonIdentifier: String = "marker"
+    }
+    
+    // MARK: - UI Componenets
+    
+    private let mapView = MKMapView()
+    private let timelineCardView: TimelineCardView = .init()
+    
+    // MARK: - Properties
+    
+    private var selectedAnnotation: MKAnnotation?
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupAttributes()
+        setupLayout()
+    }
+    
+    // MARK: - Functions
+    
+    func setMarker(by cardList: TimelineCardList, day: Int) {
+        let markerAnnotations = cardList.map {
+            TLMarkerAnnotation(
+                cardInfo: $0,
+                coordinate: .init(
+                    latitude: $0.latitude,
+                    longitude: $0.longitude
+                )
+            )
+        }
+        
+        mapView.showAnnotations(markerAnnotations, animated: true)
+        mapView.addAnnotations(markerAnnotations)
+        title = "Day \(day)"
+    }
+    
+}
+
+// MARK: - Setup Functions
+
+private extension TimelineMapVC {
+    func setupAttributes() {
+        mapView.isPitchEnabled = false
+        mapView.isRotateEnabled = false
+        mapView.delegate = self
+        
+        timelineCardView.addShadow(
+            xOffset: 0,
+            yOffset: 2,
+            blur: 12,
+            color: .black,
+            alpha: 0.25
+        )
+        timelineCardView.isHidden = true
+    }
+    
+    func setupLayout() {
+        view.addSubviews(mapView, timelineCardView)
+        
+        view.subviews.forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            timelineCardView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Metric.horizontalInset),
+            timelineCardView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Metric.horizontalInset),
+            timelineCardView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - MKMapViewDelegate
+
+extension TimelineMapVC: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation { return nil }
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: Constants.annotatinonIdentifier)
+        
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: Constants.annotatinonIdentifier)
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+        annotationView?.image = TLImage.Travel.marker
+        
+        if let imageHeight = annotationView?.bounds.height {
+            annotationView?.centerOffset = .init(x: 0, y: -imageHeight / 2)
+        }
+        
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        guard let markerAnnotation = view.annotation as? TLMarkerAnnotation else { return }
+        
+        if let selectedAnnotation {
+            mapView.deselectAnnotation(selectedAnnotation, animated: true)
+        }
+        
+        view.image = TLImage.Travel.markerSelected
+        selectedAnnotation = view.annotation
+        
+        timelineCardView.setData(cardInfo: markerAnnotation.cardInfo)
+        timelineCardView.isHidden = false
+    }
+    
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        view.image = TLImage.Travel.marker
+        timelineCardView.isHidden = true
+    }
+}
+
+@available(iOS 17, *)
+#Preview {
+    let view = TimelineMapVC()
+    view.setMarker(by: TimelineSample.makeCardList(), day: 1)
+    return view
+}
