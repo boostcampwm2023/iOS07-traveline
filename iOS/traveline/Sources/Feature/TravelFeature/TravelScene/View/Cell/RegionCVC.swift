d686d1e0815365e933010b5cdbf5d956499ebfb6
kth1210 <k2hop1210@gmail.com>
2023-11-26 02:07:45 +0900

    feat #127: RegionCVC 구현
    지역 선택 바텀시트에서 사용

---
+++ iOS/traveline/Sources/Feature/TravelFeature/TravelScene/View/Cell/RegionCVC.swift
@@ -0,0 +1,58 @@
+//
+//  RegionCVC.swift
+//  traveline
+//
+//  Created by 김태현 on 11/25/23.
+//  Copyright © 2023 traveline. All rights reserved.
+//
+
+import UIKit
+
+final class RegionCVC: UICollectionViewCell {
+    
+    // MARK: - UI Components
+    
+    private let regionLabel: TLLabel = .init(font: .body1, color: TLColor.white)
+    
+    // MARK: - Properties
+    
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? TLColor.pressedMain.withAlphaComponent(0.2) : TLColor.black
+        }
+    }
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    
+    func setRegion(_ region: String) {
+        regionLabel.setText(to: region)
+    }
+}
+
+// MARK: - Setup Functions
+
+private extension RegionCVC {
+    func setupLayout() {
+        addSubview(regionLabel)
+        regionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            regionLabel.topAnchor.constraint(equalTo: topAnchor, constant: 14.0),
+            regionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16.0),
+            regionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16.0),
+            regionLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -14.0)
+        ])
+    }
+}
