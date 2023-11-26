9fefb0174c6a26bbda258ef761906c3b09df0e6d
kmi0817 <kmi0817@naver.com>
2023-11-26 14:01:55 +0900

    chore: synchronize 속성 true → false로 변경

--- BE/src/database/database.providers.ts
+++ BE/src/database/database.providers.ts
@@ -12,7 +12,7 @@ export const databaseProviders = [
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
+        synchronize: false,
       });
       return dataSource.initialize();
     },
