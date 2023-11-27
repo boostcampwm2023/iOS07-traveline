8f0c05598c424b4c69bb42cf7b0451e39b8f46cb
yaongmeow <minnie_00@naver.com>
2023-11-27 11:23:00 +0900

    docs: swagger를 위한 코드 수정

--- BE/src/users/dto/user-name.dto.ts
+++ BE/src/users/dto/user-name.dto.ts
@@ -1,16 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
-export class UserInfoDto {
+export class UserNameDto {
   @IsOptional()
   @IsString()
   @MinLength(1)
   @MaxLength(14)
   @ApiProperty()
   name: string;
-
-  @IsString()
-  @IsOptional()
-  @ApiProperty()
-  avatar: string;
 }
--- BE/src/users/users.controller.ts
+++ BE/src/users/users.controller.ts
@@ -9,13 +9,20 @@ import {
   UseGuards,
   Req,
 } from '@nestjs/common';
-import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UserInfoDto } from './dto/user-info.dto';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CheckDuplicatedNameResponseDto } from './dto/check-duplicated-name-response.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { UserNameDto } from './dto/user-name.dto';
 
 @UseGuards(AuthGuard)
 @Controller('users')
@@ -24,9 +31,11 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @ApiBearerAuth()
   @ApiOperation({
     summary: 'id에 해당하는 User 정보 반환',
-    description: 'id에 해당하는 User 정보를 반환한다.',
+    description:
+      'id에 해당하는 User 정보를 반환합니다. avatar는 프로필 사진의 URL에 해당하는 값입니다.',
   })
   @ApiOkResponse({
     description: 'OK',
@@ -37,31 +46,39 @@ export class UsersController {
   }
 
   @Put()
+  @ApiBearerAuth()
   @ApiOperation({
     summary: 'id에 해당하는 User 정보 수정',
-    description: 'id에 해당하는 User 정보를 수정한다.',
+    description:
+      'id에 해당하는 User 정보를 수정합니다. avatar는 새로운 프로필 사진의 URL에 해당하는 값입니다.',
   })
   @ApiOkResponse({
     description: 'OK',
     type: UserInfoDto,
   })
   @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    type: UserNameDto,
+    description:
+      'json과 함께 새로운 프로필 사진을 전송해주세요. ' +
+      '프로필사진 변경사항이 없을 경우 파일을 전송하지 않으시면 됩니다. ' +
+      '닉네임 변경사항이 없을 경우 기존 닉네임을 전송해주시면 됩니다.',
+  })
   update(
     @Req() request,
-    @Body() updateUserDto: UserInfoDto,
+    @Body() userNameDto: UserNameDto,
     @UploadedFile() file: Express.Multer.File
   ) {
-    return this.usersService.updateUserInfo(
-      request['user'].id,
-      updateUserDto,
-      file
-    );
+    const name = userNameDto.name;
+    return this.usersService.updateUserInfo(request['user'].id, name, file);
   }
 
   @Get('duplicate')
+  @ApiBearerAuth()
   @ApiOperation({
     summary: 'name 중복 검사',
-    description: 'name과 동일한 이름이 DB에 존재하는지 확인한다.',
+    description: 'name과 동일한 이름이 DB에 존재하는지 확인합니다.',
   })
   @ApiOkResponse({
     description: 'OK',
