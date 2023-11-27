6ac9ec18532f34f6057e49e7809eee78ea0134ef
kmi0817 <kmi0817@naver.com>
2023-11-27 10:42:53 +0900

    docs: authorization 추가 & trapture → traveline 수정

--- BE/src/swagger/swagger.setting.ts
+++ BE/src/swagger/swagger.setting.ts
@@ -3,9 +3,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication): void {
   const config = new DocumentBuilder()
-    .setTitle('Trapture Swagger API')
+    .setTitle('traveline Swagger API')
     .setDescription('네이버커넥트재단 부스트캠프 8기 팀 프로젝트')
     .setVersion('1.0.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'accessToken'
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
