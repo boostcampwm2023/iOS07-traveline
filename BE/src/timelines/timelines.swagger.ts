af55a6f711f1a4f0553aea2aafd8d21301b0781a
kmi0817 <kmi0817@naver.com>
2023-11-29 22:50:46 +0900

    docs: create에 대한 반환 결과 작성

---
+++ BE/src/timelines/timelines.swagger.ts
@@ -0,0 +1,49 @@
+export const create_OK = {
+  title: 'from st',
+  day: 1,
+  description:
+    'we arrived Seoul Station early, so we waited while eating in the market...',
+  image: null,
+  coordX: null,
+  coordY: null,
+  date: '2023-08-16',
+  place: 'GS25',
+  time: '07:25',
+  posting: {
+    id: '9093b3bd-2730-49fd-a783-f4fd055e3f93',
+    title: 'bread ❤️❤️',
+    createdAt: '2023-11-28T09:24:06.790Z',
+    thumbnail: null,
+    startDate: '2023-08-16',
+    endDate: '2023-08-18',
+    days: 3,
+    period: '2박 3일',
+    headcount: '5인 이상',
+    budget: '10 - 50만 원',
+    location: '대전',
+    season: '여름',
+    vehicle: null,
+    theme: ['힐링', '맛집'],
+    withWho: ['가족'],
+    writer: {
+      id: '123456789012345678901234567890123456',
+      name: 'lemon',
+      avatar: null,
+      resourceId: 'temp',
+      socialType: 1,
+    },
+    liked: [
+      {
+        user: '123456789012345678901234567890123456',
+        posting: '9093b3bd-2730-49fd-a783-f4fd055e3f93',
+      },
+    ],
+    report: [
+      {
+        reporter: '000056789012345678901234567890123456',
+        posting: '9093b3bd-2730-49fd-a783-f4fd055e3f93',
+      },
+    ],
+  },
+  id: 'f983e062-9b15-4c43-a09f-72bb2bd4e99d',
+};
