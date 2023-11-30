d88db3d622149e04f46ee2e5cc2d0c1c64458b8c
kth1210 <k2hop1210@gmail.com>
2023-11-30 13:08:55 +0900

    add #169: Posting EndPoint 추가

---
+++ iOS/traveline/Sources/Data/Network/API/PostingEndPoint.swift
@@ -0,0 +1,31 @@
+//
+//  PostingEndPoint.swift
+//  traveline
+//
+//  Created by 김태현 on 11/30/23.
+//  Copyright © 2023 traveline. All rights reserved.
+//
+
+import Foundation
+
+enum PostingEndPoint {
+    case specificPosting
+}
+
+extension PostingEndPoint: EndPoint {
+    var path: String {
+        return "/postings"
+    }
+    
+    var httpMethod: HTTPMethod {
+        return .GET
+    }
+    
+    var body: Encodable? {
+        return nil
+    }
+    
+    var header: [String: String] {
+        return [:]
+    }
+}
