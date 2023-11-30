3d6079709c8daaac6f5cdf6ec615ddaf42bb9d81
kth1210 <k2hop1210@gmail.com>
2023-11-30 18:00:01 +0900

    feat #190: PostingRepositoryMock 추가

---
+++ iOS/traveline/Sources/Data/Repository/Mock/PostingRepositoryMock.swift
@@ -0,0 +1,20 @@
+//
+//  PostingRepositoryMock.swift
+//  traveline
+//
+//  Created by 김태현 on 11/30/23.
+//  Copyright © 2023 traveline. All rights reserved.
+//
+
+import Foundation
+
+final class PostingRepositoryMock: PostingRepository {
+    
+    func fetchPostingList() async throws -> TravelList {
+        try await Task.sleep(nanoseconds: 1_000_000_000)
+        
+        let mockData = TravelListSample.make()
+        return mockData
+    }
+    
+}
