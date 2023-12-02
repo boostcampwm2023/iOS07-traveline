b02cc161713eb5fe11386efc73ad473f7f5ef24e
yaongmeow <minnie_00@naver.com>
2023-12-03 00:35:08 +0900

    feat: auth guard에서 access token 내의 id 정보를 이용해 실제 회원 여부 검증

--- BE/src/auth/auth.module.ts
+++ BE/src/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { UsersModule } from 'src/users/users.module';
 import { UsersService } from 'src/users/users.service';
 import { StorageService } from 'src/storage/storage.service';
 import { HttpModule } from '@nestjs/axios';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [UsersModule, HttpModule],
   controllers: [AuthController],
-  providers: [AuthService, UsersService, StorageService],
+  providers: [AuthService, UsersService, StorageService, AuthGuard],
 })
 export class AuthModule {}
