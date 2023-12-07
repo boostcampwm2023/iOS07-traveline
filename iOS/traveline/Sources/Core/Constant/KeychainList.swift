954e2900f01564e307b7ca7bce7b34d907884f4f
otoolz <otool.hf@gmail.com>
2023-12-07 14:47:30 +0900

    feat #266: keychainList 항목 추가

--- iOS/traveline/Sources/Core/Constant/KeychainList.swift
+++ iOS/traveline/Sources/Core/Constant/KeychainList.swift
@@ -11,4 +11,6 @@ import Foundation
 enum KeychainList {
     @KeychainWrapper<String>(key: "accessToken") static var accessToken
     @KeychainWrapper<String>(key: "refreshToken") static var refreshToken
+    @KeychainWrapper<String>(key: "identityToken") static var identityToken
+    @KeychainWrapper<String>(key: "authorizationCode") static var authorizationCode
 }
