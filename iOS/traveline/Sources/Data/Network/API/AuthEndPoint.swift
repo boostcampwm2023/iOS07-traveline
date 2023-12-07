a1012a2e55bf2296c5d8146c8045e93e417107d9
otoolz <otool.hf@gmail.com>
2023-12-07 14:49:41 +0900

    feat #266: 세팅 화면 레포지토리 수정

--- iOS/traveline/Sources/Data/Network/API/AuthEndPoint.swift
+++ iOS/traveline/Sources/Data/Network/API/AuthEndPoint.swift
@@ -9,23 +9,44 @@
 import Foundation
 
 enum AuthEndPoint {
-    case withdrawal
+    case login(LoginRequestDTO)
+    case withdrawal(WithdrawRequestDTO)
 }
 
 extension AuthEndPoint: EndPoint {
     var path: String? {
-        return "/auth/withdrawal"
+        switch self {
+        case .withdrawal:
+            return "/auth/withdrawal"
+        case .login:
+            return "/auth/login"
+        }
     }
     
     var httpMethod: HTTPMethod {
-        return .DELETE
+        switch self {
+        case .withdrawal:
+            return .DELETE
+        case .login:
+            return .POST
+        }
     }
     
     var body: Encodable? {
-        return nil
+        switch self {
+        case .withdrawal(let requestDTO):
+            return requestDTO
+        case .login(let idToken):
+            return idToken
+        }
     }
     
     var header: [String: String] {
-        return HeaderType.json.value
+        switch self {
+        case .withdrawal:
+            return HeaderType.authorization.value
+        case .login:
+            return HeaderType.json.value
+        }
     }
 }
