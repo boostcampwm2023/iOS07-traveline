7706dbdcb012148703a8ef21709144b695afa161
yaongmeow <minnie_00@naver.com>
2023-12-08 00:27:52 +0900

    fix: 사진 파일 용량 10mb로 조정

--- BE/src/timelines/timelines.controller.ts
+++ BE/src/timelines/timelines.controller.ts
@@ -158,7 +158,7 @@ export class TimelinesController {
 
   @Put(':id')
   @UseInterceptors(
-    FileInterceptor('image', { limits: { fileSize: 1024 * 1024 * 2 } })
+    FileInterceptor('image', { limits: { fileSize: 1024 * 1024 * 10 } })
   )
   @ApiOperation({
     summary: 'id에 해당하는 타임라인 수정',
