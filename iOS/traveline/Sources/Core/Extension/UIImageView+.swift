44b5f98838eeede2a8d1a2b3efa7053f7aff54bc
kth1210 <k2hop1210@gmail.com>
2023-12-13 16:02:01 +0900

    fix #363: setImage 시에 ImagePath 필드 추가

--- iOS/traveline/Sources/Core/Extension/UIImageView+.swift
+++ iOS/traveline/Sources/Core/Extension/UIImageView+.swift
@@ -11,20 +11,27 @@ import UIKit
 extension UIImageView {
     
     /// urlString으로부터 이미지를 Load 합니다.
+    /// imagePath를 Key로 사용합니다.
     /// 캐시에 존재한다면 캐시에서 가져오고, 없으면 다운로드합니다.
-    /// - Parameter urlString: 이미지 URL 문자열
-    func setImage(from urlString: String?) {
+    /// - Parameters:
+    ///   - urlString: 이미지 URL 문자열
+    ///   - imagePath: 이미지 캐싱 키, nil 시에는 다운로드만 진행
+    func setImage(from urlString: String?, imagePath: String?) {
         guard let urlString else { return }
         
-        if let cachedImageData = TLImageCache.shared.fetch(urlString) {
+        if let imagePath,
+           let cachedImageData = TLImageCache.shared.fetch(imagePath) {
             image = UIImage(data: cachedImageData)
             return
         }
         
         Task {
             guard let downloadImageData = await TLImageDownloader.shared.download(key: self, urlString: urlString) else { return }
-            TLImageCache.shared.store(downloadImageData, urlString: urlString)
             image = UIImage(data: downloadImageData)
+            
+            if let imagePath {
+                TLImageCache.shared.store(downloadImageData, imagePath: imagePath)
+            }
         }
     }
     
