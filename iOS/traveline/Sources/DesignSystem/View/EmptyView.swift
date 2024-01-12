fe4a0858b7698aa29cbcf3dfb17a8492827e0042
otoolz <otool.hf@gmail.com>
2024-01-12 16:40:23 +0900

    feat #384: 엠티뷰 관련 이미지 추가

---
+++ iOS/traveline/Sources/DesignSystem/View/EmptyView.swift
@@ -0,0 +1,21 @@
+//
+//  EmptyView.swift
+//  traveline
+//
+//  Created by KiWoong Hong on 2024/01/12.
+//  Copyright © 2024 traveline. All rights reserved.
+//
+
+import UIKit
+
+class EmptyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
