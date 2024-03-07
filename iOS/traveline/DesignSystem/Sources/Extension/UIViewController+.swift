878c60104531114cf2b6b2ba796735374b7ea3c0
kth1210 <k2hop1210@gmail.com>
2024-03-07 18:55:52 +0900

    refactor #430: UIViewController extension 위치 변경
    showToast 메서드가 DesignSystem에 포함

--- iOS/traveline/DesignSystem/Sources/Extension/UIViewController+.swift
+++ iOS/traveline/DesignSystem/Sources/Extension/UIViewController+.swift
@@ -1,9 +1,8 @@
 //
 //  UIViewController+.swift
-//  traveline
+//  DesignSystem
 //
-//  Created by 김태현 on 1/16/24.
-//  Copyright © 2024 traveline. All rights reserved.
+//  Created by 김태현 on 3/7/24.
 //
 
 import UIKit
@@ -15,8 +14,16 @@ extension UIViewController {
     ///   - message: 노출할 메세지
     ///   - type: 토스트 메세지 타입 (실패, 성공)
     ///   - followsUndockedKeyboard: 키보드 위치를 트래킹하려면 true로 설정
-    public func showToast(message: String, type: TLToastView.ToastType, followsUndockedKeyboard: Bool = false) {
-        let toast: TLToastView = .init(type: type, message: message, followsUndockedKeyboard: followsUndockedKeyboard)
+    public func showToast(
+        message: String,
+        type: TLToastView.ToastType,
+        followsUndockedKeyboard: Bool = false
+    ) {
+        let toast: TLToastView = .init(
+            type: type,
+            message: message,
+            followsUndockedKeyboard: followsUndockedKeyboard
+        )
         view.keyboardLayoutGuide.followsUndockedKeyboard = followsUndockedKeyboard
         toast.show(in: view)
     }
