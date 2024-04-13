ee8ba3b93152d7d94c621af6e71fc0566ce856eb
김영인 <0inn1220@gmail.com>
2024-04-13 16:58:34 +0900

    feat #446: 차단하기 UI 및 기능 구현

--- iOS/traveline/Sources/Feature/TimelineFeature/TimelineScene/VC/TimelineVC.swift
+++ iOS/traveline/Sources/Feature/TimelineFeature/TimelineScene/VC/TimelineVC.swift
@@ -22,11 +22,6 @@ final class TimelineVC: UIViewController {
         }
     }
     
-    private enum Constants {
-        static let didFinishDeleteWithSuccess: String = "여행 삭제를 완료했어요 !"
-        static let didFinishDeleteWithFailure: String = "여행 삭제에 실패했어요."
-    }
-    
     private enum TimelineSection: Int {
         case travelInfo
         case timeline
@@ -135,6 +130,9 @@ final class TimelineVC: UIViewController {
             ]
         } else {
             menuItems = [
+                .init(title: Literal.Action.block, attributes: .destructive, handler: { [weak self] _ in
+                    self?.viewModel.sendAction(.blockTravel)
+                }),
                 .init(title: Literal.Action.report, attributes: .destructive, handler: { [weak self] _ in
                     self?.viewModel.sendAction(.reportTravel)
                 })
