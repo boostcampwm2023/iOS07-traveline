bdf2164b02a20e42aa64af2cd3f384e2bef53595
kmi0817 <kmi0817@naver.com>
2024-04-19 21:42:04 +0900

    feat: 유저 차단 테이블 Block 생성

---
+++ BE/src/users/entities/block.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { User } from './user.entity';
+
+@Entity()
+export class Block {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => User, (user) => user.blockers, { nullable: false })
+  @JoinColumn({ name: 'blocker', referencedColumnName: 'id' })
+  blocker: string;
+
+  @ManyToOne(() => User, (user) => user.blockeds, { nullable: false })
+  @JoinColumn({ name: 'blocked', referencedColumnName: 'id' })
+  blocked: string;
+}
